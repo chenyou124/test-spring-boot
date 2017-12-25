822c45ee2f4187b1a23478bf2926fe974fd238e0
zealotTL <zealotTL@users.noreply.github.com>
2017-12-25 21:41:35 +0800

    logback再修改，可打包成jar 指定方法运行

--- logback/src/main/java/name/taolei/zealot/test/springboot/logback/Main.java
+++ logback/src/main/java/name/taolei/zealot/test/springboot/logback/Main.java
@@ -6,18 +6,13 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public void helpMethod() {
-        logger.debug("This is a debug message");
-        logger.info("This is an info message");
-        logger.warn("This is a warn message");
-        logger.error("This is an error message");
-    }
-
     public static void main(String[] ags) {
-        Main Main = new Main();
+        Main main = new Main();
         for (int i = 0; i < 10; i++) {
-            Main.helpMethod();
+            main.logger.debug("This is a debug message");
+            main.logger.info("This is an info message");
+            main.logger.warn("This is a warn message");
+            main.logger.error("This is an error message");
         }
-        System.out.println("asdfasdfadsf");
     }
 }
